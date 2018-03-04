0d07e3c7010ca697b1108d1b99aac9ed0a0b5dd0
xliu43 <36863407+xliu43@users.noreply.github.com>
2018-03-03 17:46:49 -0700

    finished version

--- JS Object/App1/App1/js/main.js
+++ JS Object/App1/App1/js/main.js
@@ -8,6 +8,7 @@ window.onload = function () {
     fInit();
     document.getElementById('next').onclick = FNext;
     document.getElementById('previous').onclick = FPrev;
+    document.getElementById('play').onclick = FAuto;
 };
 
 function PicFrame(name, id)
@@ -16,14 +17,14 @@ function PicFrame(name, id)
     this.imageNumber = id;
     this.p_image = new Image();   //Image() not image()!!!  Is Image() system type
     this.p_image.src = "images/img" + (this.imageNumber + 1) + ".jpg";
-    this.p_image.imagename = this.displayName;
+    this.p_image.imagename = name;                         
     this.viewCounter = 0;
     
 }
 
 function fInit() { //iterate thur the prebulit and build the imObj based on PicFrame CTOR and push the obj to imgArr.
     for (var i = 0; i < strArr.length; i++) {
-        var imgObj = new PicFrame(strArr[0], i);
+        var imgObj = new PicFrame(strArr[i], i);
         imgArr.push(imgObj);
     }
     ShowPic();
@@ -34,18 +35,35 @@ function ShowPic() {
     var imgObj = imgArr[currentIndex];
     document.getElementById('image').setAttribute('src', imgObj.p_image.src);
     imgObj.viewCounter++;
+    document.getElementById('picIntro').innerHTML = imgObj.p_image.imagename + " This pic has been displayed " + imgObj.viewCounter;
 }
 
 /*FNext() - increment the current index ( wrapping if the last element ), and invoke showPic(). No control constructs
 are allowed ( if, else, etc), use only arithmetic only ), picture count must not be retrieved from the image object array.*/
 function FNext() {
     currentIndex++;
-    console.log(currentIndex);
+    currentIndex = (currentIndex + imgArr.length) % imgArr.length;                                                                       //notice how arithmatic algor works to warp data from 5 to 0 or from -1 to 4 
+    console.log("currentImgArr Index: "+currentIndex);
     ShowPic();
 }
 function FPrev() {
     currentIndex--;
-    console.log(currentIndex);
+    currentIndex = (currentIndex + imgArr.length) % imgArr.length;
+    console.log("currentImgArr Index: " + currentIndex);
     ShowPic();
+}
+
+function FAuto() {                                                                                                                         //remember how to use timer and toggle button
+    if (!autoFlag) {
+        timerID = window.setInterval(FNext, 500);
+        document.getElementById('play').setAttribute('value', 'Stop');
+        autoFlag = true;
+    }
+    else {
+        window.clearInterval(timerID);
+        document.getElementById('play').setAttribute('value', 'Play');
+        autoFlag = false;
+    }
+
 }
 
